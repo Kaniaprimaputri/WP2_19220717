9af24ec7bb42f15757d16aaec5e713ca0a3212ee
kaniaprimaputri <pkania888@gmail.com>
2023-10-13 15:33:26 +0700

    penambahan file baru

---
+++ controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama Saya Kania Prima Putri<br>
+        <br>Saya tinggal di Bekasi<br>
+        <br>olah raga yang saya sukai adalah berenang<br>";
+    }
+}
---
+++ controllers/Latihan1.php
@@ -0,0 +1,10 @@
+<?php
+
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"Selamat Datang...<br>
+        <br>selamat belajar Web Programming<br>";
+    }
+}
